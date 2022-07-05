1502fde93024a0384a00e49d096186830cdcab7d
franky3553 <franky3553@gmail.com>
2022-07-06 00:57:31 +0200

    Add js selections

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 //using selectors inside the element
 // traversing the dom
+
+// selections
+const btns = document.querySelectorAll('.question-btn')
+
+btns.forEach(function(btn){
+   btn.addEventListener('click', function(e){
+      const question = e.currentTarget.parentElement.parentElement;
+   })
+});
+
+
