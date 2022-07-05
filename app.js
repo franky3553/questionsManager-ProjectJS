9e797fa32fd0626e4c23bef8a18249a63b6d4ee7
franky3553 <franky3553@gmail.com>
2022-07-06 00:59:23 +0200

    Point show text event code

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const btns = document.querySelectorAll('.question-btn')
 btns.forEach(function(btn){
    btn.addEventListener('click', function(e){
       const question = e.currentTarget.parentElement.parentElement;
+      // show text event
       question.classList.toggle('show-text');
    })
 });
